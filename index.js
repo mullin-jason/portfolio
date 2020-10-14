59f4697362981c7f44829b6b8f491ebe02029c9b
Jason Mullin <jlmullin12@gmail.com>
2020-10-14 12:26:02 -0400

    heroku fix

--- index.js
+++ index.js
@@ -27,5 +27,4 @@ app.get('/resume', (req,res) => {
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
